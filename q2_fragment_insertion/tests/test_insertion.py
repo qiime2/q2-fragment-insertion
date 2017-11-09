7c7a6ff544b0bc2f10b9e6f9e753c2fc00d41bc2
Stefan Janssen <stefan.m.janssen@gmail.com>
2017-11-09 12:40:27 -0800

    fixing existing test and adding two more to test if reference validation is working

--- q2_fragment_insertion/tests/test_insertion.py
+++ q2_fragment_insertion/tests/test_insertion.py
@@ -10,23 +10,55 @@ import unittest
 
 from qiime2.sdk import Artifact
 from qiime2.plugin.testing import TestPluginBase
-from q2_types.feature_data import DNAFASTAFormat, DNAIterator
+from q2_types.feature_data import DNAFASTAFormat
 from q2_fragment_insertion._insertion import sepp_16s_greengenes
 import skbio
+from q2_types.feature_data import (AlignedDNAFASTAFormat, DNAIterator)
+from q2_types.tree import NewickFormat
 
 
-class TestDenoiseUtil(TestPluginBase):
+class TestFragmentInsertion(TestPluginBase):
     package = 'q2_fragment_insertion.tests'
 
     def test_exercise_sepp_16s_greengenes(self):
         ar = Artifact.load(self.get_data_path('real_data.qza'))
         view = ar.view(DNAFASTAFormat)
-        obs_tree, obs_placements = sepp_16s_greengenes(view)
+
+        ar_refphylo = Artifact.load(self.get_data_path(
+            'reference_phylogeny_small.qza'))
+        ref_phylo_small = ar_refphylo.view(NewickFormat)
+
+        ar_refaln = Artifact.load(self.get_data_path(
+            'reference_alignment_small.qza'))
+        ref_aln_small = ar_refaln.view(AlignedDNAFASTAFormat)
+
+        obs_tree, obs_placements = sepp_16s_greengenes(
+            view,
+            reference_alignment=ref_aln_small,
+            reference_phylogeny=ref_phylo_small)
 
         tree = skbio.TreeNode.read(str(obs_tree))
         obs = {n.name for n in tree.tips()}
-        seqs = {r.id for r in ar.view(DNAIterator)}
-        self.assertIn(seqs, obs)
+        seqs = {r.metadata['id'] for r in ar.view(DNAIterator)}
+        for seq in seqs:
+            self.assertIn(seq, obs)
+
+    def test_refmismatch(self):
+        ar_refphylo = Artifact.load(self.get_data_path(
+            'reference_phylogeny_small.qza'))
+        ref_phylo_small = ar_refphylo.view(NewickFormat)
+
+        ar_refaln = Artifact.load(self.get_data_path(
+            'reference_alignment_small.qza'))
+        ref_aln_small = ar_refaln.view(AlignedDNAFASTAFormat)
+
+        with self.assertRaises(ValueError):
+            obs_tree, obs_placements = sepp_16s_greengenes(
+                None, reference_phylogeny=ref_phylo_small)
+
+        with self.assertRaises(ValueError):
+            obs_tree, obs_placements = sepp_16s_greengenes(
+                None, reference_alignment=ref_aln_small)
 
 
 if __name__ == '__main__':
