cb20777b56f2239cdf5254f0d2e89aef81177b02
Stefan Janssen <stefan.m.janssen@gmail.com>
2018-06-12 17:25:20 +0200

    exposing new function filter_features

--- q2_fragment_insertion/__init__.py
+++ q2_fragment_insertion/__init__.py
@@ -7,8 +7,10 @@
 # ----------------------------------------------------------------------------
 import pkg_resources
 
-from ._insertion import sepp, classify_paths, classify_otus_experimental
+from ._insertion import (sepp, classify_paths, classify_otus_experimental,
+                         filter_features)
 
 
 __version__ = pkg_resources.get_distribution('q2-fragment-insertion').version
-__all__ = ['sepp', 'classify_paths', 'classify_otus_experimental']
+__all__ = ['sepp', 'classify_paths', 'classify_otus_experimental',
+           'filter_features']
