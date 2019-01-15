c468890b48de05bd14a0e73ec936590e8764c754
Matthew Dillon <matthewrdillon@gmail.com>
2019-01-15 11:39:10 -0700

    SQUASH

--- ci/recipe/build.sh
+++ ci/recipe/build.sh
@@ -8,12 +8,16 @@ tar xjvf vendor/sepp-refs/silva/sepp-package-silva.tar.bz sepp-package-silva/ref
 tar xjvf vendor/sepp-refs/silva/sepp-package-silva-2.tar.bz sepp-package-silva/ref/
 tar xjvf vendor/sepp-refs/gg/sepp-package.tar.bz sepp-package/
 
-# we have an indent error in one of the sepp python source files, which gets fixed here
-patch sepp-package/sepp/sepp/algorithm.py < indent.patch
-
 mv sepp-package-silva/ref/reference-99_otus_aligned_masked1977.fasta-rooted.tre sepp-package/ref/silva12.8_99otus_aligned_masked1977.tre
 mv sepp-package-silva/ref/RAxML_info.99_otus_aligned_masked1977.fasta           sepp-package/ref/silva12.8_99otus_aligned_masked1977.info
 mv sepp-package-silva/ref/99_otus_aligned_masked1977.fasta                      sepp-package/ref/silva12.8_99otus_aligned_masked1977.fasta
+
+if [ "$(uname)" == "Darwin" ]; then
+    rm -rf sepp-package/sepp/tools/bundled/Linux
+fi
+
+# we have an indent error in one of the sepp python source files, which gets fixed here
+patch sepp-package/sepp/sepp/algorithm.py < indent.patch
 
 # change DIR in SEPP wrapper to be relocatable
 patch sepp-package/run-sepp.sh < reloc.patch
